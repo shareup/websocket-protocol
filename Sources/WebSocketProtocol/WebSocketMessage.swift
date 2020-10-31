81e0e6410587b8603628108b355fabda8f9f7aca
Anthony Drendel <me@anthonydrendel.com>
2020-10-31 22:46:26 +0100

    Rename WebSocketMesage.data to .binary

--- Sources/WebSocketProtocol/WebSocketMessage.swift
+++ Sources/WebSocketProtocol/WebSocketMessage.swift
@@ -3,6 +3,6 @@ import Foundation
 /// An enumeration of the types of messages that can be sent and received.
 public enum WebSocketMessage: Hashable {
     case open
-    case data(Data)
+    case binary(Data)
     case text(String)
 }
