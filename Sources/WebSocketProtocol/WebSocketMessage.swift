1aaafaf0219a64bf177d684115452e84d49d7345
Anthony Drendel <atdrendel@users.noreply.github.com>
2020-11-01 12:59:53 +0100

    Conform WebSocketMessage to CustomStringConvertible (#2)

--- Sources/WebSocketProtocol/WebSocketMessage.swift
+++ Sources/WebSocketProtocol/WebSocketMessage.swift
@@ -1,8 +1,16 @@
 import Foundation
 
 /// An enumeration of the types of messages that can be sent and received.
-public enum WebSocketMessage: Hashable {
+public enum WebSocketMessage: CustomStringConvertible, Hashable {
     case open
     case binary(Data)
     case text(String)
+
+    public var description: String {
+        switch self {
+        case .open: return "open"
+        case let .binary(data): return String(data: data, encoding: .utf8) ?? ""
+        case let .text(text): return text
+        }
+    }
 }
